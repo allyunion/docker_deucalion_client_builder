175f8733c2162ae25490773ccaa10ebf88949a63
Jason Y. Lee <jylee@cs.ucr.edu>
2024-08-03 02:38:16 -0700

    Third pass

--- build_all.sh
+++ build_all.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 # Build all images in sequential order
-docker build -t ltsc2019vsbuildtools -m 2GB ./ltsc2019vsbuildtools
-docker build -t ltsc2019vsbtnet4.8 -m 2GB ./ltsc2019vsbtnet4.8
-docker build -t ltsc2019vsbtnet4.8chocolatey -m 2GB  ./ltsc2019vsbtnet4.8chocolatey
-docker build -t ltsc2019vsbtnet4.8chocorust -m 2GB ./ltsc2019vsbtnet4.8chocorust
-docker build -t ltsc2019vsbtnet4.8builder -m 2GB ./ltsc2019vsbtnet4.8builder
-docker build -t deucalion_client -m 2GB ./deucalion
+docker build -t ltsc2019vsbuildtools ./ltsc2019vsbuildtools
+docker build -t ltsc2019vsbtnet4.8 ./ltsc2019vsbtnet4.8
+docker build -t ltsc2019vsbtnet4.8chocolatey  ./ltsc2019vsbtnet4.8chocolatey
+docker build -t ltsc2019vsbtnet4.8chocorust ./ltsc2019vsbtnet4.8chocorust
+docker build -t ltsc2019vsbtnet4.8chocorustdep ./ltsc2019vsbtnet4.8chocorustdep
+docker build -t ltsc2019vsbtnet4.8builder ./ltsc2019vsbtnet4.8builder
+docker build -t deucalion_client ./deucalion
