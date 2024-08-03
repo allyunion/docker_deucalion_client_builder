2cac7b6cf89e746e4f1b233f6622e8fd155ff5d2
Jason Y. Lee <jylee@cs.ucr.edu>
2024-08-02 19:44:34 -0700

    First pass attempt

---
+++ build_all.bat
@@ -0,0 +1,9 @@
+@echo off
+
+:: Build all images in sequential order
+docker build -t ltsc2019vsbuildtools .\ltsc2019vsbuildtools
+docker build -t ltsc2019vsbtnet4.8 .\ltsc2019vsbtnet4.8
+docker build -t ltsc2019vsbtnet4.8chocolatey .\ltsc2019vsbtnet4.8chocolatey
+docker build -t ltsc2019vsbtnet4.8chocorust .\ltsc2019vsbtnet4.8chocorust
+docker build -t ltsc2019vsbtnet4.8builder .\ltsc2019vsbtnet4.8builder
+docker build -t deucalion_client .\deucalion
